1edcb5f986e465d29189c3425dd32c984340edb0
Bob-Maximus <114927549+Bob-Maximus@users.noreply.github.com>
2024-11-27 12:01:26 -0500

    added sum things

---
+++ Code.js
@@ -0,0 +1,29 @@
+
+var gamePiece;
+
+function startGame() {
+    myGameArea.start();
+    thing = new component(10, 10, "red", 0, 0);
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
+
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
